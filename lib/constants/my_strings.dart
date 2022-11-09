86964dce446edcc7c8b6bda1402a992075bfa57d
Naji Ezzeldien <aboalnaja10@gmail.com>
2022-11-09 20:08:50 +0200

    first commit

--- lib/constants/my_strings.dart
+++ lib/constants/my_strings.dart
@@ -1,7 +1,7 @@
 const loginScreen = '/login-screen';
 const otpScreen = '/otp-screen';
 const mapScreen = '/map-screen';
-const googleApiKey = "AIzaSyDVZW8Fqvo6BSYPVkrskuLLanIqRt7ReCk";
+const googleApiKey = "Your googleApiKey";
 const suggestionBaseUrl =
     "https://maps.googleapis.com/maps/api/place/autocomplete/json";
 const placeLocationBaseUrl =
